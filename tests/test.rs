aca0c7be7a1de54638537153feb4b5d5d6cad12d
周睿 <zrufo747@outlook.com>
2024-12-02 14:58:22 +0800

    init project

---
+++ tests/test.rs
@@ -0,0 +1,31 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(bare_test::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use bare_test::{driver::device_tree::get_device_tree, mem::mmu::iomap, println};
+
+bare_test::test_setup!();
+
+#[test_case]
+fn it_works1() {
+    println!("test1... ");
+    assert_eq!(1, 1);
+}
+
+#[test_case]
+fn test_uart() {
+    // map uart data register for using.
+    let uart_data_reg = iomap(0x9000000.into(), 0x1000);
+
+    let fdt = get_device_tree().unwrap();
+
+    // write to uart, then it will be print to the screen.
+    unsafe {
+        uart_data_reg.write_volatile(b'A');
+        uart_data_reg.write_volatile(b'\n');
+    }
+
+    println!("uart test passed!");
+}
